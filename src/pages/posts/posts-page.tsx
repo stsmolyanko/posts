42dea47f4d1e9b7233a7198c55a0f8e9fb03f52f
StanislavSmolyanko <st.smolyanko@gmail.com>
2023-03-03 12:14:13 +0100

    added material UI

--- src/pages/posts/posts-page.tsx
+++ src/pages/posts/posts-page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
 export const PostsPage = () => {
   return (
     <div>
       <h1>Hello, Posts!</h1>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 };
